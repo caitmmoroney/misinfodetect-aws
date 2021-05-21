f3cc8d1c5a139ed77889e0d810a211fc01879aea
Caitlin Moroney <57643150+caitmmoroney@users.noreply.github.com>
2021-05-21 11:23:50 -0400

    only run experiments for SVM w last two kernels

--- Experiments.py
+++ Experiments.py
@@ -231,7 +231,7 @@ if __name__ == '__main__':
 		return result_latex
 
 	models = ['ICA', 'NMF', 'DL', 'IVA-mean', 'IVA-max', 'LDA', 'bert-large-uncased', 'bert-large-cased', 'bert-base-uncased', 'bert-base-cased', 'roberta-base', 'roberta-large', 'google/electra-base-discriminator', 'google/electra-large-discriminator']
-	kernels = ['rbf', 'linear', 'poly', 'sigmoid']
+	kernels = ['poly', 'sigmoid']#['rbf', 'linear', 'poly', 'sigmoid']
 	for kernel in kernels:
 		complete_scores = dict()
 		for mname in models:
